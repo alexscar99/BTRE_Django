1d44214f555eefb3af0092285f18f646d55e1a4f
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-14 04:56:34 -0400

    admin page now has a checkbox for the is published column that will change the db value based on checked or not

--- listings/admin.py
+++ listings/admin.py
@@ -8,6 +8,7 @@ class ListingAdmin(admin.ModelAdmin):
                     'list_date', 'realtor')
     list_display_links = ('id', 'title')
     list_filter = ('realtor', )
+    list_editable = ('is_published', )
     search_fields = ('title', 'description', 'address', 'city', 'state',
                      'zip_code', 'price')
     list_per_page = 25
