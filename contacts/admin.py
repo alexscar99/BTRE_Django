bd1ee5d703f26b4ff811935202a0ad6dade8d6bf
Alex Scarlett <alex.scarlett91@gmail.com>
2018-10-15 03:46:59 -0400

    Configured app for sending emails when an inquiry is made, dropped and migrated contacts table back, fixed random settings, hid variables in a different way, contacts app edits

--- contacts/admin.py
+++ contacts/admin.py
@@ -5,7 +5,7 @@ from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'listing', 'email', 'contact_date')
-    lilst_display_links = ('id', 'name')
+    list_display_links = ('id', 'name')
     search_fields = ('name', 'email', 'listing')
     list_per_page = 25
 
